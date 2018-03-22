a8bb09a7ec473fce10ed3e3411dd657e0409c6d9
JonghyunOh <korzakk@gmail.com>
2018-03-22 19:14:23 -0400

    Show multiple tasks on popup.

--- Comp213002SchedulerApplication/Comp213002SchedulerApplication/reschedulePopup.aspx.cs
+++ Comp213002SchedulerApplication/Comp213002SchedulerApplication/reschedulePopup.aspx.cs
@@ -12,6 +12,8 @@ namespace Comp213002SchedulerApplication
 {
     public partial class reschedulePopup : System.Web.UI.Page
     {
+
+        List<dailyTaskHandler> taskList = new List<dailyTaskHandler>();
         
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -36,15 +38,52 @@ namespace Comp213002SchedulerApplication
             ad.Fill(ds);
             foreach (DataRow row in ds.Tables[0].Rows)
             {
-                this.lbContents.Text = "<br>Task Name : " + row["subject"];
-                this.lbContents.Text += ("<br>Assignor : " + row["userName"]);
-                this.lbContents.Text += ("<br>Priority : " + row["Priority"]);
-                this.lbContents.Text += ("<br>Status : " + row["Status"]);
-                this.lbContents.Text += ("<br>Schedule Start : " + row["ScheduleStart"]);
-                this.lbContents.Text += ("<br>Schedule End : " + row["ScheduleEnd"]);
-                this.lbContents.Text += "<br><br>";
+                dailyTaskHandler dth = new dailyTaskHandler();
+                dth.taskName = (string)row["subject"];
+                dth.assignor = (string)row["userName"];
+                dth.priority = (string)row["Priority"];
+                dth.status = (string)row["Status"];
+                dth.startDate = ((System.DateTime)row["ScheduleStart"]).ToLongDateString();
+                dth.endDate = ((System.DateTime)row["ScheduleEnd"]).ToShortDateString();
+
+                taskList.Add(dth);
             }
+            makeAString();
+
             myConnection.Close();
         }
+
+        public void makeAString()
+        {
+
+            string vTaskName = taskList.Select(l_taskName => l_taskName.taskName).Aggregate((current, next) => current + ", " + next);
+            string vAssignor = taskList.Select(l_Assignor => l_Assignor.assignor).Aggregate((current, next) => current + ", " + next);
+            string vPriority = taskList.Select(l_Priority => l_Priority.priority).Aggregate((current, next) => current + ", " + next);
+            string vStatus = taskList.Select(l_status => l_status.status).Aggregate((current, next) => current + ", " + next);
+            string vStartDate = taskList.Select(l_startDate => l_startDate.startDate).Aggregate((current, next) => current + ", " + next);
+            string vEndDate = taskList.Select(l_endDate => l_endDate.endDate).Aggregate((current, next) => current + ", " + next);
+
+            this.lbContents.Text = "<br>Task Name : " + vTaskName;
+            this.lbContents.Text += "<br>Assignor : " + vAssignor;
+            this.lbContents.Text += "<br>Priority : " + vPriority;
+            this.lbContents.Text += "<br>Status : " + vStatus;
+            this.lbContents.Text += "<br>Schedule Start : " + vStartDate;
+            this.lbContents.Text += "<br>Schedule End : " + vEndDate;
+            this.lbContents.Text += "<br><br>";
+        }
+
+
+        public class dailyTaskHandler
+        {
+            public string taskName { get; set; }
+            public string assignor { get; set; }
+            public string priority { get; set; }
+            public string status { get; set; }
+            public string startDate { get; set; }
+            public string endDate { get; set; }
+
+        }
     }
+
+
 }
