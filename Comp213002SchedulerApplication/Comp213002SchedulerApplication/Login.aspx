8985e172163735858c8bbb11d1a1ed2c68c4de29
Mingi <Mingi@DESKTOP-2MK3NBG>
2018-03-24 14:16:24 -0400

    Codes

--- Comp213002SchedulerApplication/Comp213002SchedulerApplication/Login.aspx
+++ Comp213002SchedulerApplication/Comp213002SchedulerApplication/Login.aspx
@@ -1,3 +1,47 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Comp213002SchedulerApplication.Login" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+        <div>
+            <div>
+                  <table id="tbl1">       
+                         <tr><td colspan="2">
+                             <h4 id="LogoName">ESMS</h4></td></tr> 
+
+                         <tr><td rowspan="3">
+                             <img src="Asset/MainLogo.jpg" /></td><td class="auto-style2"> 
+        
+                                 <h4>Please Enter your detail to log in</h4>
+                                 <div class="col-md-6 divBody heightRegistration" id="loginDiv">
+
+                <div id="Div1" runat="server" visible="false">
+                    <h1>Welcome</h1>
+                    <p>User : <asp:Label ID="Username" runat="server" Text="Label" BackColor="Red"></asp:Label></p>
+                   </div>
+        <div id="Div2" runat="server" visible="true">
+
+                     
+        <h2>Login</h2>
+        <table>
+        <tr><td>Username:</td><td> <asp:TextBox runat="server" ID="loginUsernameTB"></asp:TextBox>
+                              </td></tr>
+
+        <tr><td>Password:</td><td> <asp:TextBox runat="server" ID="loginPasswordTB" TextMode="Password"></asp:TextBox>
+                              </td></tr>
+            </table>
+        <br />
+        <asp:Button CssClass="btn" runat="server" Text="Login" ID="btnLogin" OnClick="Login_Click"/>
+        <asp:Label runat="server" ID="WarningLblLogin"></asp:Label>
+            
+
+  
+                            </div>
+                        </div>
+                                           
+                         </td></tr> 
+                         
+        
+                         </table>
+        
+         
+                </div>
+        </div>
 </asp:Content>
--- Comp213002SchedulerApplication/Comp213002SchedulerApplication/Login.aspx.cs
+++ Comp213002SchedulerApplication/Comp213002SchedulerApplication/Login.aspx.cs
@@ -1,16 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace Comp213002SchedulerApplication
 {
     public partial class Login : System.Web.UI.Page
     {
+
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["esmsDbConnectionStr"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
+            bool isAuthenticated = (HttpContext.Current.User != null) && HttpContext.Current.User.Identity.IsAuthenticated;
+
+            SqlCommand com = new SqlCommand("SELECT * FROM UserInfo", conn);
+
+
+
+            if (Session["User"] != null)
+            {
+                // nologin.Visible = false;
+                Username.Text = (string)(Session["User"]);
+                //login.Visible = true;
+            }
+
+            if (isAuthenticated == true)
+            {
+                //nologin.Visible = false;
+            }
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            /*Check user credentical and login*/
+
+            SqlCommand checkUser = new SqlCommand("Select UserId FROM [dbo].[UserInfo] WHERE UserId = @username", conn);
+            SqlCommand checkPassword = new SqlCommand("Select Password FROM [dbo].[UserInfo] WHERE Password = @password", conn);
+
+            checkUser.Parameters.Add("@username", SqlDbType.NVarChar);
+            checkUser.Parameters["@username"].Value = loginUsernameTB.Text;
+
+            checkPassword.Parameters.Add("@password", SqlDbType.NVarChar);
+            checkPassword.Parameters["@password"].Value = loginPasswordTB.Text;
+
+
+            try
+            {
+                conn.Open();
+                string username = checkUser.ExecuteScalar().ToString();
+
+                if (username != null && String.Equals(username, loginUsernameTB.Text))
+                {
+                    object returnValue = checkPassword.ExecuteScalar();
+                    if (returnValue == null)
+                    {
+                        return;
+                    }
+                    string password = returnValue.ToString();
+
+                    if (password != null && String.Equals(password, loginPasswordTB.Text))
+                    {
+                        FormsAuthentication.SetAuthCookie(username, false);
+                        //nologin.Visible = false;
+                        Session["User"] = loginUsernameTB.Text;
+                        Response.Redirect("~/WebForm1.aspx");
+
+                    }
+
+                }
+                else
+                {
+                    WarningLblLogin.Text = "No username was found";
+                }
+
+            }
+            catch (Exception exception)
+            {
+                WarningLblLogin.Text = exception.Message.ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+
 
         }
     }
