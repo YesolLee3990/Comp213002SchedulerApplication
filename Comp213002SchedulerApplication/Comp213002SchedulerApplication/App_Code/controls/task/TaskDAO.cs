a5c2a3eecd4ee0712ce4818b705f96e1ed35ea40
sam <sam@SAM-HP>
2018-03-26 19:37:55 -0400

    Login4

--- Comp213002SchedulerApplication/Comp213002SchedulerApplication/App_Code/controls/task/TaskDAO.cs
+++ Comp213002SchedulerApplication/Comp213002SchedulerApplication/App_Code/controls/task/TaskDAO.cs
@@ -8,7 +8,6 @@ using System.Web;
 
 namespace Comp213002SchedulerApplication.App_Code.controls.task {
     public class TaskDAO{
-        string columns = "ID, USERINFO_ID, ASSIGNOR";
         public Task GetTask(int id) {
             return DBUtil.SelectOne<Task>("SELECT * FROM TASK WHERE ID = '" + id + "'");
         }
