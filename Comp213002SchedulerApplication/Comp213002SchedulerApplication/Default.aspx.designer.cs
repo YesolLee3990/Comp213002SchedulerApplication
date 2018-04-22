6aea3169225a9cb082640fb81eba62b5b38cc0ce
Mingi <Mingi@DESKTOP-2MK3NBG>
2018-04-22 14:08:49 -0400

    ssss

--- Comp213002SchedulerApplication/Comp213002SchedulerApplication/Default.aspx.designer.cs
+++ Comp213002SchedulerApplication/Comp213002SchedulerApplication/Default.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace Comp213002SchedulerApplication {
         protected global::System.Web.UI.WebControls.Button assignTaskBtn;
         
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// Calendar1 control.
         /// </summary>
         /// <remarks>
